390048fb48d94d774bb2eccc273ec29de76b1ca8
Rohan <rohan1das1@gmail.com>
2024-05-03 05:46:57 +0530

    Updated

---
+++ MyContract.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.12 <0.9.0;
+
+contract MyContract{
+
+    string value;
+
+    constructor() {
+        value = "myValue";
+    }
+
+    function get() public view returns(string memory)
+    {
+        return value;
+    }
+
+    function set(string memory _value) public{
+        value = _value;
+    }
+
+
+}
